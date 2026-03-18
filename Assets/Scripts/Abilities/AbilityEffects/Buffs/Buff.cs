59752bec791eb00b2286e8b8f5be85dc1be89e98
Elias dridi <e.dridibourguiba@gmail.com>
2026-03-18 01:58:47 +0100

    [Add] Few name changed and Buff/Debuff added as Interface

--- Assets/Scripts/Abilities/AbilityEffects/Buffs/Buff.cs
+++
@@ -1,9 +0,0 @@
-using Enums;
-
-namespace Abilities.AbilityEffects.Buffs
-{
-    public abstract class Buff : EffectOverTime
-    {
-        public override EffectType EffectType => EffectType.Buff;
-    }
-}
--- Assets/Scripts/Abilities/AbilityEffects/Debuffs/Debuff.cs
+++
@@ -1,9 +0,0 @@
-using Enums;
-
-namespace Abilities.AbilityEffects.Debuffs
-{
-    public abstract class Debuff : EffectOverTime
-    {
-        public override EffectType EffectType => EffectType.Debuff;
-    }
-}
